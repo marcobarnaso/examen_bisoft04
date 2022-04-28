7f57c1e51cc539e7282d3e95c8c7e21f46660be3
Marvin Rojas <kdashcr@gmail.com>
2022-04-28 17:45:50 -0600

    configuring static serve in express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require('path')
 const express = require("express")
 const app = express()
 const cors = require('cors')
@@ -9,6 +10,35 @@ app.use(cors())
 app.use(platilloRouter)
 
 const port = 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('views', path.join(__dirname, '../templates/views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res)=> {
+    res.render('landing', {
+        title: 'Landing',
+        name: 'Marvin Rojas'
+    })
+})
+
+app.get('/registro', (req, res)=> {
+    res.render('registro', {
+        title: 'Landing',
+        name: 'Marvin Rojas'
+    })
+})
+
+app.get('/lista', (req, res)=> {
+    res.render('menu', {
+        title: 'Landing',
+        name: 'Marvin Rojas'
+    })
+})
 
 app.listen(port, () => {
     console.log(`El Servidor local está arriba y andando en el puerto ${port}.`);
